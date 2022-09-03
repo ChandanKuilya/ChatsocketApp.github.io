7246eb36292da7bc12fc73a769e749e080866656
Chandan Kuilya <67276477+ChandanKuilya@users.noreply.github.com>
2022-09-03 23:10:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const hostname= '0.0.0.0';
+
 const path = require("path");
 
 const app = express();
@@ -6,7 +8,7 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
-const PORT= 5000;
+const PORT= 3000;
 app.use(express.static(path.join(__dirname+"/public")));
 
 io.on("connection", function(socket){
