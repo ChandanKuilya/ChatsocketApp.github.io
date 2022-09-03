fb6ea8ebaf6f7a5d6cd11cfea751398272c26998
ChandanKuilya <67276477+ChandanKuilya@users.noreply.github.com>
2022-09-03 19:03:43 +0530

    commit first
    my first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+const PORT= 5000;
+app.use(express.static(path.join(__dirname+"/public")));
+
+io.on("connection", function(socket){
+	socket.on("newuser",function(username){
+		socket.broadcast.emit("update", username + " joined the conversation");
+	});
+	socket.on("exituser",function(username){
+		socket.broadcast.emit("update", username + " left the conversation");
+	});
+	socket.on("chat",function(message){
+		socket.broadcast.emit("chat", message);
+	});
+});
+
+//server.listen(5000);
+
+//const PORT=5000;
+server.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
